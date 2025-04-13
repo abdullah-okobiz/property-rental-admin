120f98ac90f69c4abd24c817b13eff414cf1aaa1
abdullah-okobiz <abdullah.okobiz@gmail.com>
2025-04-13 10:53:18 +0600

    fix(refreshToken): refreshToken auto removing issue fixed

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -9,62 +9,49 @@ const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   useEffect(() => {
-    const token = localStorage.getItem("accessToken");
-    if (token) {
-      try {
-        const data = jwtDecode(token);
-        setUser(data);
-        setIsAuthenticated(true);
-      } catch (err) {
-        console.error("Invalid token", err);
-        localStorage.removeItem("accessToken");
-        setIsAuthenticated(false);
+    (async () => {
+      const accessToken = localStorage.getItem("accessToken");
+      if (accessToken) {
+        try {
+          const decoded = jwtDecode(accessToken);
+          if (decoded.exp * 1000 > Date.now()) {
+            setUser(decoded);
+            setIsAuthenticated(true);
+          } else {
+            await refreshToken();
+          }
+        } catch {
+          await refreshToken();
+        }
+      } else {
+        await refreshToken();
       }
-    }
-    // (async () => {
-    //   const accessToken = localStorage.getItem("accessToken");
-    //   if (accessToken) {
-    //     try {
-    //       const decoded = jwtDecode(accessToken);
-    //       if (decoded.exp * 1000 > Date.now()) {
-    //         setUser(decoded);
-    //         setIsAuthenticated(true);
-    //       } else {
-    //         await refreshToken();
-    //       }
-    //     } catch {
-    //       await refreshToken();
-    //     }
-    //   } else {
-    //     await refreshToken();
-    //   }
-    // })();
+    })();
   }, []);
 
   const login = ({ accessToken }) => {
     localStorage.setItem("accessToken", accessToken);
     const data = jwtDecode(accessToken);
-    console.log(data);
     setUser(data);
     setIsAuthenticated(true);
   };
-  // const refreshToken = async () => {
-  //   try {
-  //     const res = await processrRefreshToken();
-  //     const { accessToken } = res.data;
-  //     localStorage.setItem("accessToken", accessToken);
-  //     const decoded = jwtDecode(accessToken);
-  //     setUser(decoded);
-  //     setIsAuthenticated(true);
-  //   } catch (err) {
-  //     logout();
-  //   }
-  // };
-  // const logout = () => {
-  //   localStorage.removeItem("accessToken");
-  //   setUser(null);
-  //   setIsAuthenticated(false);
-  // };
+  const refreshToken = async () => {
+    try {
+      const res = await processrRefreshToken();
+      const { accessToken } = res;
+      localStorage.setItem("accessToken", accessToken);
+      const decoded = jwtDecode(accessToken);
+      setUser(decoded);
+      setIsAuthenticated(true);
+    } catch (err) {
+      logout();
+    }
+  };
+  const logout = () => {
+    localStorage.removeItem("accessToken");
+    setUser(null);
+    setIsAuthenticated(false);
+  };
   return (
     <AuthContext.Provider value={{ user, setUser, login, isAuthenticated }}>
       {children}
